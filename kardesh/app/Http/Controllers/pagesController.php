f06cc98ced5eb962168126e7604d34f0d650c380
rashid collins <rashid@localhost.localdomain>
2020-05-26 20:00:24 +0300

    setup

--- kardesh/app/Http/Controllers/pagesController.php
+++ kardesh/app/Http/Controllers/pagesController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class pagesController extends Controller
 {
-    //
+    public function about(Request $request)
+    {
+     return view('pages.about');   
+    }
+
+    public function courses(Request $request)
+    {
+        return view('pages.courses');
+    }
+
+    public function donate(Request $request)
+    {
+        return view('pages.donate');
+    }
+
+    public function blog(Request $request)
+    {
+        return view('pages.blog');
+    }
+
+    public function gallery(Request $request)
+    {
+        return view('pages.gallery');
+    }
+
+    public function events(Request $request)
+    {
+        return view('pages.events');   
+    }
+
+    public function contact(Request $request)
+    {
+        return view('pages.contact');
+    }
+
 }
--- kardesh/routes/web.php
+++ kardesh/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout');
+    return view('welcome');
 });
 
 Auth::routes();
@@ -24,3 +24,23 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// frontend routes below here
+Route::get('/about', 'pagesController@about')->name('about');
+Route::get('/courses', 'pagesController@courses')->name('courses');
+Route::get('/donate', 'pagesController@donate')->name('donate');
+Route::get('/blog', 'pagesController@blog')->name('blog');
+Route::get('/gallery', 'pagesController@gallery')->name('gallery');
+Route::get('/events', 'pagesController@events')->name('events');
+Route::get('/contact', 'pagesController@contact')->name('contact');
+
+
+// backend routes below here
+
+
+// api intergration routes below here
+
+
+
+// madam grace routes below here
+
